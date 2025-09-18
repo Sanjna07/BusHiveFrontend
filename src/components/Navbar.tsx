801937c4a2a34cdc892605353ca2ccd1a1024d16
Rudrakshi Rai <bennettrigel23@gmail.com>
2025-09-18 21:34:33 +0530

    Updated Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -15,7 +15,7 @@ const Navbar: React.FC = () => {
             <div className="bg-orange-500 p-2 rounded-lg">
               <Bus className="w-6 h-6 text-white" />
             </div>
-            <img src="\logo.jpeg" alt="Logo" width="150" height="80">
+            {/* <img src="\logo.jpeg" alt="Logo" width="150" height="80"> */} 
 
             {/* <span className="text-xl font-bold text-orange-600">BusHive</span> */} 
           </Link>
