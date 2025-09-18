071686c6909fd15b3927e61a6be92cc8a3297e2d
Rudrakshi Rai <bennettrigel23@gmail.com>
2025-09-18 21:47:59 +0530

    Updated Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -13,7 +13,7 @@ const Navbar: React.FC = () => {
           {/* Logo */}
           <Link to="/" className="flex items-center">
             <img 
-              src="/logo.jpeg" 
+              src="\logo.jpeg" 
               alt="Logo" 
               className="h-20 w-auto" // responsive size
             />
